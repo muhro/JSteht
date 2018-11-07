3348f2d0485ecd2e69c29920fa57c116e9f011b6
unknown <lauriaus@metropolia.fi>
2018-11-07 12:06:07 +0200

    message

--- JSteht6/js/teht3.js
+++ JSteht6/js/teht3.js
@@ -6,35 +6,32 @@ const numero2 = document.querySelector('input[name=numero2]');
 const submit = document.querySelector('input[name=submit]');
 const p = document.querySelector('p');
 
-document.getElementById("sum").addEventListener("click", A());
-document.getElementById("sub").addEventListener("click", B());
-document.getElementById("multi").addEventListener("click", C());
-document.getElementById("div").addEventListener("click", D());
-
-function A() {
+document.getElementById("sum").addEventListener("click", function() {
   form.onsubmit = function(evt) {
     const summa = +numero1.value + +numero2.value;
     evt.preventDefault();
     p.innerText = 'yhteenlaskusi tulos on ' + +summa;
-  }
-}
 
-function B() {
+  }
+});
+
+document.getElementById("sub").addEventListener("click", function() {
   form.onsubmit = function(evt) {
     const ext = +numero1.value - +numero2.value;
     evt.preventDefault();
     p.innerText = 'vähennyslaskusi tulos on ' + +ext;
   }
-}
-function C() {
+});
+document.getElementById("multi").addEventListener("click", function() {
   form.onsubmit = function(evt) {
     evt.preventDefault();
     p.innerText = ' kertolaskusi tulos on ' + numero1.value * numero2.value;
   }
-}
-function D() {
+});
+document.getElementById("divv").addEventListener("click", function() {
   form.onsubmit = function(evt) {
     evt.preventDefault();
     p.innerText = 'jakolaskus tulos on ' + numero1.value / numero2.value;
   }
-}
+});
+
